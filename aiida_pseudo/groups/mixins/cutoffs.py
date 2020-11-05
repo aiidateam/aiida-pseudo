7996f05603bcb2174cc9251cdf3ca78083848385
Sebastiaan Huber <mail@sphuber.net>
2020-11-05 23:19:46 +0100

    `RecommendedCutoffMixin`: add concept of stringency levels
    Up till now, the `RecommendedCutoffMixin` allowed to define one single
    set of recommended cutoffs per family. However, sometimes it makes sense
    to define multiple sets of cutoffs for a single family, each that
    represent a different level of computational stringency with respect to
    the precision of the results that they produce.
    
    The interface is updated to allow defining multiple sets of cutoffs. At
    the same time a default stringency has to be defined such that the
    normal interface `get_cutoffs` can continue to function without a caller
    having to know which stringency to pass. The `get_cutoff_stringencies`
    allows a caller to inspect which stringencies are available on the
    family.

--- aiida_pseudo/groups/mixins/cutoffs.py
+++ aiida_pseudo/groups/mixins/cutoffs.py
@@ -11,64 +11,136 @@ __all__ = ('RecommendedCutoffMixin',)
 
 
 class RecommendedCutoffMixin:
-    """Mixin that adds support of recommended cutoffs to a ``Group`` subclass, using its extras."""
+    """Mixin that adds support of recommended cutoffs to a ``Group`` subclass, using its extras.
+
+    This class assumes that the cutoffs apply to a plane-wave based code and as such the cutoffs pertain to the wave
+    functions and the charge density.
+    """
 
     _key_cutoffs = '_cutoffs'
+    _key_default_stringency = '_default_stringency'
 
     @classmethod
     def validate_cutoffs(cls, elements: set, cutoffs: dict) -> None:
         """Validate a cutoff dictionary for a given set of elements.
 
         :param elements: set of elements for which to validate the cutoffs dictionary.
-        :param cutoffs: dictionary with recommended cutoffs. Format: keys are the element symbols and the values are
-            dictionaries themselves, each with two keys, ``cutoff_wfc`` and ``cutoff_rho``, containing a float value
-            with the recommended cutoff to be used for the wave functions and charge density, respectively.
+        :param cutoffs: dictionary with recommended cutoffs. Format: multiple sets of recommended cutoffs can be
+            specified where the key represents the stringency, e.g. ``low`` or ``normal``. For each stringency, a
+            dictionary should be defined that for each element symbols for which the family contains a pseudopotential,
+            two values are specified, ``cutoff_wfc`` and ``cutoff_rho``, containing a float value with the recommended
+            cutoff to be used for the wave functions and charge density, respectively.
         :raises ValueError: if the set of elements and those defined in the cutoffs do not match exactly, or if the
             cutoffs dictionary has an invalid format.
         """
         elements_family = set(elements)
-        elements_cutoffs = set(cutoffs.keys())
-        elements_diff = elements_family ^ elements_cutoffs
 
-        if elements_family < elements_cutoffs:
-            raise ValueError(f'cutoffs defined for unsupported elements: {elements_diff}')
+        for stringency, cutoffs_sub in cutoffs.items():
 
-        if elements_family > elements_cutoffs:
-            raise ValueError(f'cutoffs not defined for all family elements: {elements_diff}')
+            elements_cutoffs = set(cutoffs_sub.keys())
+            elements_diff = elements_family ^ elements_cutoffs
 
-        for element, values in cutoffs.items():
-            if set(values.keys()) != {'cutoff_wfc', 'cutoff_rho'}:
-                raise ValueError(f'invalid cutoff keys for element {element}: {values}')
+            if elements_family < elements_cutoffs:
+                raise ValueError(f'cutoffs defined for unsupported elements: {elements_diff}')
 
-            if any(not isinstance(cutoff, (int, float)) for cutoff in values.values()):
-                raise ValueError(f'invalid cutoff values for element {element}: {values}')
+            if elements_family > elements_cutoffs:
+                raise ValueError(f'cutoffs not defined for all family elements: {elements_diff}')
 
-    def set_cutoffs(self, cutoffs: dict) -> None:
-        """Set the recommended cutoffs for the pseudos in this family.
+            for element, values in cutoffs_sub.items():
+                if set(values.keys()) != {'cutoff_wfc', 'cutoff_rho'}:
+                    raise ValueError(
+                        f'invalid cutoff keys for stringency `{stringency}` and element {element}: {values}'
+                    )
 
-        :param cutoffs: dictionary with recommended cutoffs. Format: keys are the element symbols and the values are
-            dictionaries themselves, each with two keys, ``cutoff_wfc`` and ``cutoff_rho``, containing a float value
-            with the recommended cutoff to be used for the wave functions and charge density, respectively.
-        :raises ValueError: if the cutoffs have an invalid format
+                if any(not isinstance(cutoff, (int, float)) for cutoff in values.values()):
+                    raise ValueError(
+                        f'invalid cutoff values for stringency `{stringency}` and element {element}: {values}'
+                    )
+
+    def validate_stringency(self, stringency: str) -> None:
+        """Validate a cutoff stringency.
+
+        :param stringency: the cutoff stringency to validate.
+        :raises ValueError: if stringency is None or the family does not define cutoffs for the specified stringency.
         """
-        self.validate_cutoffs(set(self.elements), cutoffs)
-        self.set_extra(self._key_cutoffs, cutoffs)
+        if stringency is None:
+            raise ValueError('defining a stringency is required.')
 
-    def get_cutoffs(self) -> Union[dict, None]:
+        if stringency not in self.get_cutoff_stringencies():
+            raise ValueError(f'stringency `{stringency}` is not defined for this family.')
+
+    def _get_cutoffs(self) -> dict:
         """Return the cutoffs dictionary.
 
-        :return: the cutoffs or ``None`` if they are not defined for this family.
+        :return: the cutoffs extra or an empty dictionary if it has not yet been set.
         """
-        return self.get_extra(self._key_cutoffs, None)
+        return self.get_extra(self._key_cutoffs, {})
 
-    def get_recommended_cutoffs(self, *, elements=None, structure=None):
+    def get_default_stringency(self) -> Union[str, None]:
+        """Return the default stringency if defined.
+
+        :return: the default stringency or ``None`` if no cutoffs have been defined for this family.
+        :raises ValueError: if not default stringency has been defined.
+        """
+        try:
+            return self.get_extra(self._key_default_stringency)
+        except AttributeError as exception:
+            raise ValueError('no default stringency has been defined.') from exception
+
+    def get_cutoff_stringencies(self) -> tuple:
+        """Return a tuple of the available cutoff stringencies.
+
+        :return: the tuple of stringencies that are defined for this family.
+        """
+        return tuple(self._get_cutoffs().keys())
+
+    def set_cutoffs(self, cutoffs: dict, default_stringency: str = None) -> None:
+        """Set the recommended cutoffs for the pseudos in this family.
+
+        :param cutoffs: dictionary with recommended cutoffs. Format: one or multiple sets of recommended cutoffs can be
+            specified where each key represents a stringency set, e.g. ``low`` or ``normal``. For each stringency, a
+            dictionary should be defined that for each element symbols for which the family contains a pseudopotential,
+            two values are specified, ``cutoff_wfc`` and ``cutoff_rho``, containing a float value with the recommended
+            cutoff to be used for the wave functions and charge density, respectively.
+        :param default_stringency: the default stringency to be used when ``get_recommended_cutoffs`` is called. If is
+            possible to not specify this if and only if the cutoffs only contain a single stringency set. That one will
+            then automatically be set as default.
+        :raises ValueError: if the cutoffs have an invalid format or the default stringency is invalid.
+        """
+        self.validate_cutoffs(set(self.elements), cutoffs)
+
+        if default_stringency is None and len(cutoffs) != 1:
+            raise ValueError('have to explicitly specify a default stringency when specifying multiple cutoff sets.')
+
+        default_stringency = default_stringency or list(cutoffs.keys())[0]
+
+        self.set_extra(self._key_cutoffs, cutoffs)
+        self.set_extra(self._key_default_stringency, default_stringency)
+
+    def get_cutoffs(self, stringency=None) -> Union[dict, None]:
+        """Return a set of cutoffs for the given stringency.
+
+        :param stringency: optional stringency if different from the default.
+        :return: the cutoffs or ``None`` if no cutoffs whatsoever have been defined for this family.
+        :raises ValueError: if the requested stringency is not defined for this family.
+        """
+        stringency = stringency or self.get_default_stringency()
+
+        try:
+            return self._get_cutoffs()[stringency]
+        except KeyError as exception:
+            raise ValueError(f'stringency `{stringency}` is not defined for this family.') from exception
+
+    def get_recommended_cutoffs(self, *, elements=None, structure=None, stringency=None):
         """Return tuple of recommended wavefunction and density cutoffs for the given elements or ``StructureData``.
 
         .. note:: at least one and only one of arguments ``elements`` or ``structure`` should be passed.
 
         :param elements: single or tuple of elements.
         :param structure: a ``StructureData`` node.
+        :param stringency: optional stringency if different from the default.
         :return: tuple of recommended wavefunction and density cutoff.
+        :raises ValueError: if the requested stringency is not defined for this family.
         """
         if (elements is None and structure is None) or (elements is not None and structure is not None):
             raise ValueError('at least one and only one of `elements` or `structure` should be defined')
@@ -85,7 +157,7 @@ class RecommendedCutoffMixin:
 
         cutoffs_wfc = []
         cutoffs_rho = []
-        cutoffs = self.get_cutoffs()
+        cutoffs = self.get_cutoffs(stringency=stringency)
 
         for element in symbols:
             values = cutoffs[element]
--- tests/groups/mixins/test_cutoffs.py
+++ tests/groups/mixins/test_cutoffs.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# pylint: disable=redefined-outer-name
 """Tests for the :mod:`aiida_pseudo.groups.mixins.cutoffs` module."""
 import copy
 
@@ -23,40 +24,133 @@ class CutoffFamily(RecommendedCutoffMixin, PseudoPotentialFamily, metaclass=Fixt
     """Test class for the ``RecommendedCutoffMixin``."""
 
 
+@pytest.fixture
+def get_cutoffs():
+    """Return a cutoffs dictionary."""
+
+    def _get_cutoffs(family, stringencies=('default',)):
+        cutoffs = {}
+
+        for element in family.elements:
+            cutoffs[element] = {
+                'cutoff_wfc': 1.0,
+                'cutoff_rho': 2.0,
+            }
+
+        return {stringency: cutoffs for stringency in stringencies}
+
+    return _get_cutoffs
+
+
+@pytest.mark.usefixtures('clear_db')
+def test_get_cutoffs_private(get_pseudo_family, get_cutoffs):
+    """Test the ``RecommendedCutoffMixin._get_cutoffs`` method."""
+    family = get_pseudo_family(cls=CutoffFamily)
+    assert family._get_cutoffs() == {}  # pylint: disable=protected-access
+
+    family.set_cutoffs(get_cutoffs(family), 'default')
+    assert family._get_cutoffs() == get_cutoffs(family)  # pylint: disable=protected-access
+
+
+@pytest.mark.usefixtures('clear_db')
+def test_validate_stringency(get_pseudo_family, get_cutoffs):
+    """Test the ``RecommendedCutoffMixin.validate_stringency`` method."""
+    family = get_pseudo_family(cls=CutoffFamily)
+
+    with pytest.raises(ValueError, match=r'stringency `.*` is not defined for this family.'):
+        family.validate_stringency('default')
+
+    cutoffs = get_cutoffs(family)
+    stringency = list(cutoffs.keys())[0]
+    family.set_cutoffs(cutoffs, stringency)
+
+    with pytest.raises(ValueError, match=r'stringency `.*` is not defined for this family.'):
+        family.validate_stringency(stringency + 'non-existing')
+
+    family.validate_stringency(stringency)
+
+
+@pytest.mark.usefixtures('clear_db')
+def test_get_default_stringency(get_pseudo_family, get_cutoffs):
+    """Test the ``RecommendedCutoffMixin.get_default_stringency`` method."""
+    family = get_pseudo_family(cls=CutoffFamily)
+
+    with pytest.raises(ValueError, match='no default stringency has been defined.'):
+        family.get_default_stringency()
+
+    cutoffs = get_cutoffs(family)
+    stringency = list(cutoffs.keys())[0]
+    family.set_cutoffs(cutoffs, stringency)
+
+    assert family.get_default_stringency() == stringency
+
+
+@pytest.mark.usefixtures('clear_db')
+def test_get_cutoff_stringencies(get_pseudo_family, get_cutoffs):
+    """Test the ``RecommendedCutoffMixin.get_cutoff_stringencies`` method."""
+    family = get_pseudo_family(cls=CutoffFamily)
+    assert family.get_cutoff_stringencies() == ()
+
+    stringencies = ('low', 'normal', 'high')
+    cutoffs = get_cutoffs(family, stringencies)
+    family.set_cutoffs(cutoffs, stringencies[0])
+
+    assert sorted(family.get_cutoff_stringencies()) == sorted(stringencies)
+
+
 @pytest.mark.usefixtures('clear_db')
 def test_set_cutoffs(get_pseudo_family):
     """Test the `RecommendedCutoffMixin.set_cutoffs` method."""
     elements = ['Ar', 'He']
     family = get_pseudo_family(label='SSSP/1.0/PBE/efficiency', cls=CutoffFamily, elements=elements)
-    cutoffs = {element: {'cutoff_wfc': 1.0, 'cutoff_rho': 2.0} for element in elements}
-    family.set_cutoffs(cutoffs)
+    cutoffs = {'default': {element: {'cutoff_wfc': 1.0, 'cutoff_rho': 2.0} for element in elements}}
+    family.set_cutoffs(cutoffs, 'default')
 
-    assert family.get_cutoffs() == cutoffs
+    assert family.get_cutoffs() == cutoffs['default']
 
     with pytest.raises(ValueError, match=r'cutoffs defined for unsupported elements: .*'):
-        cutoffs_invalid = copy.copy(cutoffs)
-        cutoffs_invalid['C'] = {'cutoff_wfc': 1.0, 'cutoff_rho': 2.0}
-        assert family.set_cutoffs(cutoffs_invalid)
+        cutoffs_invalid = copy.deepcopy(cutoffs)
+        cutoffs_invalid['default']['C'] = {'cutoff_wfc': 1.0, 'cutoff_rho': 2.0}
+        family.set_cutoffs(cutoffs_invalid, 'default')
 
     with pytest.raises(ValueError, match=r'cutoffs not defined for all family elements: .*'):
-        cutoffs_invalid = copy.copy(cutoffs)
-        cutoffs_invalid.pop('He')
-        assert family.set_cutoffs(cutoffs_invalid)
+        cutoffs_invalid = copy.deepcopy(cutoffs)
+        cutoffs_invalid['default'].pop('He')
+        family.set_cutoffs(cutoffs_invalid, 'default')
 
-    with pytest.raises(ValueError, match=r'invalid cutoff keys for element .*: .*'):
-        cutoffs_invalid = copy.copy(cutoffs)
-        cutoffs_invalid['He'] = {'cutoff_wfc': 1.0}
-        assert family.set_cutoffs(cutoffs_invalid)
+    with pytest.raises(ValueError, match=r'invalid cutoff keys for stringency `default` and element .*: .*'):
+        cutoffs_invalid = copy.deepcopy(cutoffs)
+        cutoffs_invalid['default']['He'] = {'cutoff_wfc': 1.0}
+        family.set_cutoffs(cutoffs_invalid, 'default')
 
-    with pytest.raises(ValueError, match=r'invalid cutoff keys for element .*: .*'):
-        cutoffs_invalid = copy.copy(cutoffs)
-        cutoffs_invalid['He'] = {'cutoff_wfc': 1.0, 'cutoff_rho': 2.0, 'cutoff_extra': 3.0}
-        assert family.set_cutoffs(cutoffs_invalid)
+    with pytest.raises(ValueError, match=r'invalid cutoff keys for stringency `default` and element .*: .*'):
+        cutoffs_invalid = copy.deepcopy(cutoffs)
+        cutoffs_invalid['default']['He'] = {'cutoff_wfc': 1.0, 'cutoff_rho': 2.0, 'cutoff_extra': 3.0}
+        family.set_cutoffs(cutoffs_invalid, 'default')
 
-    with pytest.raises(ValueError, match=r'invalid cutoff values for element .*: .*'):
-        cutoffs_invalid = copy.copy(cutoffs)
-        cutoffs_invalid['He'] = {'cutoff_wfc': 1.0, 'cutoff_rho': 'string'}
-        assert family.set_cutoffs(cutoffs_invalid)
+    with pytest.raises(ValueError, match=r'invalid cutoff values for stringency `default` and element .*: .*'):
+        cutoffs_invalid = copy.deepcopy(cutoffs)
+        cutoffs_invalid['default']['He'] = {'cutoff_wfc': 1.0, 'cutoff_rho': 'string'}
+        family.set_cutoffs(cutoffs_invalid, 'default')
+
+
+@pytest.mark.usefixtures('clear_db')
+def test_set_cutoffs_auto_default(get_pseudo_family):
+    """Test the `RecommendedCutoffMixin.set_cutoffs` method when not specifying explicit default.
+
+    If the cutoffs specified only contain a single set, the `default_stringency` is determined automatically.
+    """
+    elements = ['Ar']
+    family = get_pseudo_family(label='SSSP/1.0/PBE/efficiency', cls=CutoffFamily, elements=elements)
+    values = {element: {'cutoff_wfc': 1.0, 'cutoff_rho': 2.0} for element in elements}
+    cutoffs = {'default': values}
+
+    with pytest.raises(ValueError, match='specify a default stringency when specifying multiple cutoff sets.'):
+        family.set_cutoffs({'low': values, 'hi': values})
+
+    family.set_cutoffs(cutoffs)
+    assert family.get_cutoffs() == cutoffs['default']
+    assert family.get_default_stringency() == 'default'
 
 
 @pytest.mark.usefixtures('clear_db')
@@ -64,12 +158,17 @@ def test_get_cutoffs(get_pseudo_family):
     """Test the `RecommendedCutoffMixin.get_cutoffs` method."""
     elements = ['Ar', 'He']
     family = get_pseudo_family(label='SSSP/1.0/PBE/efficiency', cls=CutoffFamily, elements=elements)
-    cutoffs = {element: {'cutoff_wfc': 1.0, 'cutoff_rho': 2.0} for element in elements}
+    cutoffs = {'default': {element: {'cutoff_wfc': 1.0, 'cutoff_rho': 2.0} for element in elements}}
 
-    assert family.get_cutoffs() is None
+    with pytest.raises(ValueError, match='no default stringency has been defined.'):
+        family.get_cutoffs()
 
-    family.set_cutoffs(cutoffs)
-    assert family.get_cutoffs() == cutoffs
+    family.set_cutoffs(cutoffs, 'default')
+
+    with pytest.raises(ValueError, match=r'stringency `.*` is not defined for this family.'):
+        family.get_cutoffs('non-existing')
+
+    assert family.get_cutoffs() == cutoffs['default']
 
 
 @pytest.mark.usefixtures('clear_db')
@@ -77,17 +176,19 @@ def test_get_recommended_cutoffs(get_pseudo_family, generate_structure):
     """Test the `RecommendedCutoffMixin.get_recommended_cutoffs` method."""
     elements = ['Ar', 'He']
     cutoffs = {
-        'Ar': {
-            'cutoff_wfc': 1.0,
-            'cutoff_rho': 2.0
-        },
-        'He': {
-            'cutoff_wfc': 3.0,
-            'cutoff_rho': 8.0
-        },
+        'default': {
+            'Ar': {
+                'cutoff_wfc': 1.0,
+                'cutoff_rho': 2.0
+            },
+            'He': {
+                'cutoff_wfc': 3.0,
+                'cutoff_rho': 8.0
+            },
+        }
     }
     family = get_pseudo_family(label='SSSP/1.0/PBE/efficiency', cls=CutoffFamily, elements=elements)
-    family.set_cutoffs(cutoffs)
+    family.set_cutoffs(cutoffs, 'default')
     structure = generate_structure(elements=elements)
 
     with pytest.raises(ValueError):
@@ -102,17 +203,17 @@ def test_get_recommended_cutoffs(get_pseudo_family, generate_structure):
     with pytest.raises(TypeError):
         family.get_recommended_cutoffs(elements=None, structure='Ar')
 
-    expected = cutoffs['Ar']
+    expected = cutoffs['default']['Ar']
     assert family.get_recommended_cutoffs(elements='Ar') == (expected['cutoff_wfc'], expected['cutoff_rho'])
     assert family.get_recommended_cutoffs(elements=('Ar',)) == (expected['cutoff_wfc'], expected['cutoff_rho'])
 
-    expected = cutoffs['He']
+    expected = cutoffs['default']['He']
     assert family.get_recommended_cutoffs(elements=('Ar', 'He')) == (expected['cutoff_wfc'], expected['cutoff_rho'])
 
-    expected = cutoffs['He']
+    expected = cutoffs['default']['He']
     assert family.get_recommended_cutoffs(structure=structure) == (expected['cutoff_wfc'], expected['cutoff_rho'])
 
     # Try structure with multiple kinds with the same element
-    expected = cutoffs['He']
+    expected = cutoffs['default']['He']
     structure = generate_structure(elements=['He1', 'He2'])
     assert family.get_recommended_cutoffs(structure=structure) == (expected['cutoff_wfc'], expected['cutoff_rho'])

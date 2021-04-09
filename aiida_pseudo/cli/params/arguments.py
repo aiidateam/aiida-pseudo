c644a94b69b1ba51a5075d179093d278922cab27
Sebastiaan Huber <mail@sphuber.net>
2021-04-09 09:02:06 +0200

    CLI: add the `aiida-pseudo family cutoffs set` command (#55)
    This command allows to set recommended cutoffs for a given family. The
    cutoffs are specified in a JSON file that is given as an argument and
    the stringency is specified through the `-s/--stringency` option. If the
    stringency already exists, it is simply overwritten.

---
+++ aiida_pseudo/cli/params/arguments.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Reusable arguments for CLI commands."""
+from aiida.cmdline.params.arguments import OverridableArgument
+from .types import PseudoPotentialFamilyParam
+
+__all__ = ('PSEUDO_POTENTIAL_FAMILY')
+
+PSEUDO_POTENTIAL_FAMILY = OverridableArgument(
+    'family', type=PseudoPotentialFamilyParam(sub_classes=('aiida.groups:pseudo.family',))
+)

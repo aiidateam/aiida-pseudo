0a0b58a5322da8f5bdf40bf93b912d7ad1c204ef
Sebastiaan Huber <mail@sphuber.net>
2021-08-12 16:58:16 +0200

    Dependencies: put upper limit on `psycopg2-binary` (#106)
    The release `psycopg2-binary==2.9` breaks the unit test manager of
    `aiida-core`. This was fixed for in v1.6, however, that version no
    longer supports Python 3.6, which falls back on `aiida-core==1.5.2` and
    that doesn't apply the upper limit on `psycopg2-binary`, so we are
    forced to do it here. This limit can be removed once we drop support for
    Python 3.6.
    
    Also fixes two minor things brought up by `pylint`.

--- aiida_pseudo/cli/install.py
+++ aiida_pseudo/cli/install.py
@@ -356,7 +356,7 @@ def cmd_install_pseudo_dojo(
             adjusted_cutoffs = {}
             for stringency, str_cutoffs in cutoffs.items():
                 adjusted_cutoffs[stringency] = []
-                max_cutoff_wfc = max([element_cutoffs['cutoff_wfc'] for element_cutoffs in str_cutoffs.values()])
+                max_cutoff_wfc = max([cutoffs['cutoff_wfc'] for cutoffs in str_cutoffs.values()])
                 filler_cutoff_wfc = max_cutoff_wfc * 1.5
                 for element, cutoff in str_cutoffs.items():
                     if cutoff['cutoff_wfc'] <= 0:
